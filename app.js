a1391ddc46fc36cff9e8f3001f627de15932ea36
Windows <shaikjunaid124@gmail.com>
2025-08-02 12:28:56 +0530

    hella-new-feature

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get('/health', (req, res) => {
   res.send('ok')
 })
 
+app.get('/why', (req, res) => {
+    res.send('idontknoweither.')
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
